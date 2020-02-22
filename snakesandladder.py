9d474e67729d1d50f4dc132506e7d3ba1dbd172a
Niharika <guptaniharika851@gmail.com>
2020-02-22 14:48:13 +0530

    ADDED:Repeate till the Player reaches the winning position 100

--- snakesandladder.py
+++ snakesandladder.py
@@ -50,3 +50,26 @@ def got_ladder_jump(old_value, current_value, player_name):
     print("\n" + random.choice(ladder_jump).upper() + " ########")
     print("\n" + player_name + " climbed the ladder from " + str(old_value) + " to " + str(current_value))
 
+def snake_ladder(player_name, current_value, dice_value):
+    time.sleep(SLEEP_BETWEEN_ACTIONS)
+    old_value = current_value
+    current_value = current_value + dice_value
+
+    if current_value > MAX_VAL:
+        print("You need " + str(MAX_VAL - old_value) + " to win this game. Keep trying.")
+        return old_value
+
+    print("\n" + player_name + " moved from " + str(old_value) + " to " + str(current_value))
+    if current_value in snakes:
+        final_value = snakes.get(current_value)
+        got_snake_bite(current_value, final_value, player_name)
+
+    elif current_value in ladders:
+        final_value = ladders.get(current_value)
+        got_ladder_jump(current_value, final_value, player_name)
+
+    else:
+        final_value = current_value
+
+    return final_value
+
